fcb538a5d6217490ef5f1acd24a48a335cb542b7
BiteBytePKU <133830980+BiteBytePKU@users.noreply.github.com>
2024-03-19 23:41:40 +0800

    Update server.py

--- demo/server.py
+++ demo/server.py
@@ -1,11 +1,10 @@
 from langchain.vectorstores import Chroma
-import os
+from langchain_core.messages import AIMessage
 from model import miniCPM
 from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
 from modelscope import snapshot_download
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
+from langchain.chains import ConversationalRetrievalChain, ConversationChain
 import logging
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
@@ -30,61 +29,112 @@ embeddings = HuggingFaceBgeEmbeddings(
 # logger.addHandler(console_handler)
 
 path = snapshot_download('OpenBMB/miniCPM-bf16')
+# 加载自定义 LLM
+llm = miniCPM(model_path=path)
 
-def load_chain():
-    
+
+def load_RAchain():
+    # 加载问答链
+    # 定义 Embeddings
+    # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+
     # 向量数据库持久化路径
     persist_directory = '../db/data_base/chroma'
 
     # 加载数据库
     vectordb = Chroma(
-        persist_directory=persist_directory, 
+        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
         embedding_function=embeddings
     )
 
-    # 加载自定义 LLM
-    llm = miniCPM(model_path=path)
     memory = ConversationBufferMemory(
         memory_key="chat_history", output_key="answer", return_messages=True
     )
     # 定义一个 Prompt Template
-    template = """你是AISI科学智能研发的专家模型，请使用以下上下文来回答最后的问题。如果不知道答案，就说你不知道，不要编造答
-    案。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
+    template = """你是一名科学家，根据提问所用的语言来回答问题。总是在回答的最后说“谢谢提问！”。
     {chat_history}
     {context}
     问题: {question}
     回答:"""
+
     QA_CHAIN_PROMPT = PromptTemplate(input_variables=["chat_history", "context", "question"], template=template)
+
     # 运行 chain
-    chain = ConversationalRetrievalChain.from_llm(llm, memory=memory, retriever=vectordb.as_retriever(),
-                                        return_source_documents=True,
-                                        combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT})
+    chain = ConversationalRetrievalChain.from_llm(llm, memory=memory,
+                                                  retriever=vectordb.as_retriever(search_kwargs={"k": 3}),
+                                                  return_source_documents=True,
+                                                  combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT})
+
     return chain
+
+
+def load_chain():
+    # SUMMARIZER_TEMPLATE = """请将以下内容逐步概括所提供的对话内容，并将新的概括添加到之前的概括中，形成新的概括。
+    # EXAMPLE
+    # Current summary:
+    # Human询问AI对人工智能的看法。AI认为人工智能是一种积极的力量。
+    # New lines of conversation:
+    # Human：为什么你认为人工智能是一种积极的力量？
+    # AI：因为人工智能将帮助人类发挥他们的潜能。
+    # New summary:
+    # Human询问AI对人工智能的看法。AI认为人工智能是一种积极的力量，因为它将帮助人类发挥他们的潜能。
+    # END OF EXAMPLE
+    # Current summary:
+    # {summary}
+    # New lines of conversation:
+    # {new_lines}
+    # New summary:"""
+    #
+    # SUMMARY_PROMPT = PromptTemplate(
+    #     input_variables=["summary", "new_lines"],
+    #     template=SUMMARIZER_TEMPLATE
+    # )
+    memory = ConversationSummaryBufferMemory(
+        llm=llm, max_token_limit=512
+    )
+    # # 定义一个 Prompt Template
+    # template = """你是一名科学家，根据提问所用的语言来回答问题。总是在回答的最后说“谢谢提问！”。
+    # {chat_history}
+    # 问题: {question}
+    # 回答:"""
+    #
+    # CHAIN_PROMPT = PromptTemplate(input_variables=["chat_history", "question"], template=template)
+
+    # 运行 chain
+    chain = ConversationChain(llm=llm, memory=memory)
+
+    return chain
+
 
 class myChatModel():
     """
     存储检索问答链的对象
     """
+
     def __init__(self):
         # 构造函数，加载检索问答链
+        self.RAchain = load_RAchain()
         self.chain = load_chain()
 
-    def get_reply(self, question: str):
+    def get_reply(self, question: str, status):
         """
         调用问答链进行回答
         """
         if question == None or len(question) < 1:
             return ""
         try:
-            res = self.chain.invoke({"question": question})
-            # print(res)
+            if status == 2:
+                res = self.RAchain.invoke({"question": question})
+            else:
+                res = self.chain.predict(input=question)
+                print(res)
             return res
         except Exception as e:
             return e
 
-myChat = myChatModel()
 
+myChat = myChatModel()
 if __name__ == "__main__":
     chain = load_chain()
-    res = chain.invoke({"question": "北京大学"})
+    res = chain.predict(input="北京大学")
     print(res)
